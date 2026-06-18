0309ffa34f15f1246e2a60941a10162436756d60
SubZero <98052460+OmarXSub0@users.noreply.github.com>
2026-06-18 04:26:15 +0000

    Update Firebase initialization with credentials

--- Garage.py
+++ Garage.py
@@ -32,7 +32,7 @@ firebaseConfig = {
     "messagingSenderId": "405891329254", 
     "appId": "1:405891329254:web:8c511fbcccf25fd9d01f27",
 } 
-firebase_py = firebase_admin.initialize_app(firebaseConfig)
+firebase_py = firebase_admin.initialize_app(cred, firebaseConfig)
 pyre_auth = firebase_py.auth()
 
 @app.route('/static/placeholder.png')
